09db910af34e8d1666d6309e9ca490507914a115
Lauri Nuotio <lauri.nuotio@siili.com>
2019-11-05 12:23:05 +0200

    fix: append data correctly on dom

--- content.js
+++ content.js
@@ -1,82 +1,109 @@
-const address = document.querySelector(
-  ".ItemSummaryContainer__alignLeft__2IE5Z"
-).children[1].innerText;
-const items = document.querySelector(".CompactInfoRow__infoRow__2hjs_")
-  .parentNode;
-
-const appendData = data => {
-  const listItem = document.createElement("div");
-  listItem.setAttribute(
-    "class",
-    "CompactInfoRow__infoRow__2hjs_ flexboxgrid__row__wfmuy"
+const appendSkeleton = () => {
+  let bodyElem = document.getElementsByTagName("body")[0];
+  let link = document.createElement("link");
+  link.setAttribute("type", "text/css");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute(
+    "href",
+    "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css"
   );
-  const title = document.createElement("div");
-  title.setAttribute(
-    "class",
-    "flexboxgrid__col-xs-12__1I1LS flexboxgrid__col-sm-4__3RH7g ItemHeader__itemHeader__32xAv"
-  );
-  const titleText = document.createElement("em");
-  const content = document.createElement("div");
-  content.setAttribute(
-    "class",
-    "flexboxgrid__col-xs-12__1I1LS flexboxgrid__col-sm-8__2jfMv CompactInfoRow__content__3jGt4"
-  );
-
-  if (data.rows) {
-    // Create table, add heads
-    let table = document.createElement("table");
-    table.setAttribute("class", "u-full-width");
-    let thead = document.createElement("thead");
-    let tr = document.createElement("tr");
-
-    let th1 = document.createElement("th");
-    th1.innerHTML = "Paikka";
-    tr.appendChild(th1);
-
-    let th2 = document.createElement("th");
-    th2.innerHTML = "Etäisyys";
-    tr.appendChild(th2);
-
-    let th3 = document.createElement("th");
-    th3.innerHTML = "Matka-aika";
-    tr.appendChild(th3);
-
-    thead.append(tr);
-    table.appendChild(thead);
-
-    let tbody = document.createElement("tbody");
-    // Addresses
-    data.origin_addresses.forEach((elem, index) => {
-      let row = document.createElement("tr");
-      let singleAddress = document.createElement("td");
-      singleAddress.innerHTML = elem;
-      row.appendChild(singleAddress);
-
-      let singleDistance = document.createElement("td");
-      singleDistance.innerHTML = data.rows[index].elements[0].distance.text;
-      row.appendChild(singleDistance);
-
-      let singleDuration = document.createElement("td");
-      singleDuration.innerHTML = data.rows[index].elements[0].duration.text;
-      row.appendChild(singleDuration);
-      tbody.appendChild(row);
-    });
-    table.appendChild(tbody);
-    content.appendChild(table);
-  } else {
-    titleText.innerText = "Ei tietoja";
-    content.innerHTML = "Aseta paikkatietoja chromen lisäosien asetuksista";
-  }
-  title.appendChild(titleText);
-  listItem.appendChild(title);
-  listItem.appendChild(content);
-  items.insertBefore(listItem, items.firstChild);
+  bodyElem.appendChild(link);
 };
 
-if (address) {
-  chrome.runtime.sendMessage({ address: address }, response => {
-    appendData(response.data);
-  });
-} else {
-  appendData(false);
-}
+window.addEventListener("load", function() {
+  const targetAddress = document.querySelector(
+    ".ItemSummaryContainer__alignLeft__2IE5Z"
+  ).children[1].innerText;
+  const items = document.querySelector(".CompactInfoRow__infoRow__2hjs_")
+    .parentNode;
+
+  const appendData = data => {
+    const listItem = document.createElement("div");
+    listItem.setAttribute(
+      "class",
+      "CompactInfoRow__infoRow__2hjs_ flexboxgrid__row__wfmuy"
+    );
+    const title = document.createElement("div");
+    title.setAttribute(
+      "class",
+      "flexboxgrid__col-xs-12__1I1LS flexboxgrid__col-sm-4__3RH7g ItemHeader__itemHeader__32xAv"
+    );
+    const titleText = document.createElement("em");
+    const content = document.createElement("div");
+    content.setAttribute(
+      "class",
+      "flexboxgrid__col-xs-12__1I1LS flexboxgrid__col-sm-8__2jfMv CompactInfoRow__content__3jGt4"
+    );
+
+    console.log("Data", data);
+
+    if (data.length != 0) {
+      // Create table, add heads
+      let table = document.createElement("table");
+      table.setAttribute("class", "u-full-width");
+      let thead = document.createElement("thead");
+      let tr = document.createElement("tr");
+
+      let th1 = document.createElement("th");
+      th1.innerHTML = "Paikka";
+      tr.appendChild(th1);
+
+      let th2 = document.createElement("th");
+      th2.innerHTML = "Autolla";
+      tr.appendChild(th2);
+
+      let th3 = document.createElement("th");
+      th3.innerHTML = "Julkisilla";
+      tr.appendChild(th3);
+
+      let th4 = document.createElement("th");
+      th4.innerHTML = "Pyörällä";
+      tr.appendChild(th4);
+
+      thead.append(tr);
+      table.appendChild(thead);
+
+      let tbody = document.createElement("tbody");
+      // Addresses
+      data.forEach((elem, index) => {
+        let row = document.createElement("tr");
+        let singleAddress = document.createElement("td");
+        singleAddress.innerHTML = elem.address;
+        row.appendChild(singleAddress);
+
+        let bikeData = document.createElement("td");
+        bikeData.innerHTML = elem.cycling;
+        row.appendChild(bikeData);
+
+        let drivingData = document.createElement("td");
+        drivingData.innerHTML = elem.driving;
+        row.appendChild(drivingData);
+
+        let transitData = document.createElement("td");
+        transitData.innerHTML = elem.transit;
+        row.appendChild(transitData);
+
+        tbody.appendChild(row);
+      });
+      table.appendChild(tbody);
+      content.appendChild(table);
+    } else {
+      titleText.innerText = "Virhe, ei tietoja";
+      content.innerHTML =
+        "a) Onko paikat asetettu lisäosan asetuksista? b) Kokeile ladata sivu uudelleen";
+    }
+    title.appendChild(titleText);
+    listItem.appendChild(title);
+    listItem.appendChild(content);
+    items.insertBefore(listItem, items.firstChild);
+  };
+
+  if (targetAddress) {
+    chrome.runtime.sendMessage({ address: targetAddress }, response => {
+      appendData(response.data);
+      appendSkeleton();
+    });
+  } else {
+    appendData(false);
+  }
+});
--- main.js
+++ main.js
@@ -16,33 +16,87 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
   chrome.storage.sync.get(["locations"], data => {
     // Fetch form google distance matrix api
-    console.log("Data", data);
     let addressesHolder = [];
     data.locations.forEach(elem => {
-      console.log("asd", elem.address);
       addressesHolder.push(encodeURI(elem.address));
     });
 
-    let addresses = addressesHolder.join("|");
+    const modes = ["bicycling", "driving", "transit"];
+    const addresses = addressesHolder.join("|");
+    let targetAddress = encodeURI(req.address.split(",")[0]);
 
-    const targetAddress = encodeURI(req.address);
-    let apiUrl =
-      "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=" +
-      addresses +
-      "&destinations=" +
-      targetAddress +
-      "&key=APIKEY";
+    const apiUrlFactory = mode => {
+      return (apiUrl =
+        "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=" +
+        addresses +
+        "&destinations=" +
+        targetAddress +
+        "&mode=" +
+        `${mode}` +
+        `&key=YOUR_API_KEY_HERE`);
+    };
 
-    console.log("APIURL", apiUrl);
-    fetch(apiUrl)
-      .then(res => {
-        return res.json();
+    const attemptSending = responses => {
+      if (responses.length === modes.length) {
+        let destination = [];
+        let addressCurator = [];
+        responses.forEach((transportMode, transportIndex) => {
+          transportMode.rows.forEach((elem, i) => {
+            addressCurator.push(transportMode.origin_addresses[i]);
+            let combinedDistanceDuration =
+              elem.elements[0].distance.text +
+              "/" +
+              elem.elements[0].duration.text;
+            destination.push(combinedDistanceDuration);
+          });
+        });
+        const addressCount = responses[0].origin_addresses.length;
+        const addressesWithData = [];
+        console.log("Data", responses);
+
+        for (let i = 0; i < addressCount; i++) {
+          let addressInc = responses[0].origin_addresses[i];
+          let daatta = {};
+          daatta["address"] = addressInc;
+
+          daatta["cycling"] =
+            responses[0].rows[i].elements[0].distance.text +
+            "/" +
+            responses[0].rows[i].elements[0].duration.text;
+
+          daatta["driving"] =
+            responses[1].rows[i].elements[0].distance.text +
+            "/" +
+            responses[1].rows[i].elements[0].duration.text;
+
+          daatta["transit"] =
+            responses[2].rows[i].elements[0].distance.text +
+            "/" +
+            responses[2].rows[i].elements[0].duration.text;
+
+          addressesWithData.push(daatta);
+        }
+
+        sendResponse({ data: addressesWithData });
+      }
+    };
+
+    Promise.all([
+      fetch(apiUrlFactory("cycling")).then(res => res.json()),
+      fetch(apiUrlFactory("driving")).then(res => res.json()),
+      fetch(apiUrlFactory("transit")).then(res => res.json())
+    ])
+
+      .then(([cycling, driving, transit]) => {
+        const responses = [];
+        responses.push(cycling);
+        responses.push(driving);
+        responses.push(transit);
+        attemptSending(responses);
       })
-      .then(data => {
-        console.log("DATA", data);
-        sendResponse({ data: data });
-      })
-      .catch(error => console.error(error));
+      .catch(err => {
+        console.error(err);
+      });
   });
   return true;
 });
